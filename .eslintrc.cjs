86dcf1aa18c240846f2445f6476d9ef7828ca8ca
3rooks <Brookslynx@gmail.com>
2023-04-29 06:01:39 -0300

    🔧 prettier + eslint / config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+	env: {
+		browser: true,
+		es2021: true
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'standard',
+		'eslint-config-prettier'
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		'react/prop-types': 'off'
+	}
+};
